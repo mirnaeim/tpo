8793403ff7968226e9169d634ec887051adfb539
mirnaeim <mirnaimhoseyni@gmail.com>
2021-10-07 20:07:06 +0330

    completed navbar and search input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-$(window).scroll(function () {
-    let scrollTop = $(this).scrollTop()
-    if(scrollTop> 500){
-        $('nav.navbar').addClass('fixed-top')
-    }
-    else {
-        $('nav.navbar').removeClass('fixed-top')
+$(document).ready(function () {
+    if($(window).width() > 992){
+        $('.dropdown').hover(function () {
+            $(this).children('.dropdown-menu').addClass('show').attr('data-bs-popper', 'none');
+            $(this).children('.nav-link').addClass('show');
+
+        },function () {
+            $(this).children('.dropdown-menu').removeClass('show');
+            $(this).children('.nav-link').removeClass('show');
+        })
     }
 })
+
+$(window).resize(function () {
+    if($(window).width() > 992){
+        $('.dropdown').hover(function () {
+            $(this).children('.dropdown-menu').addClass('show').attr('data-bs-popper', 'none');
+            $(this).children('.nav-link').addClass('show');
+
+        },function () {
+            $(this).children('.dropdown-menu').removeClass('show');
+            $(this).children('.nav-link').removeClass('show');
+        })
+    }
+})
+
